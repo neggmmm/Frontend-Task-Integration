c71e16f92adfcd83d08199986739abc97417c980
neggmmm <kekkdead@gmail.com>
2026-02-13 13:57:39 +0200

    change color to match the theme

--- src/lib/toast.ts
+++ src/lib/toast.ts
@@ -1,9 +1,11 @@
 import { toast } from 'react-toastify'
 
 export function showToast(message: string, type: 'success' | 'error' | 'info' = 'info') {
-  toast[type](message, {
-    position: 'bottom-right',
-    closeOnClick: true,
-    pauseOnHover: false,
-  })
+    toast[type](message, {
+        position: "top-right",
+        autoClose: 2000,
+        closeOnClick: false,
+        pauseOnHover: true,
+        theme: "dark",
+    })
 }
